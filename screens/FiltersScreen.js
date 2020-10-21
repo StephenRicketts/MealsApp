b757116b1bcdc1d9a4c94d30250a7294adc799ac
StephenRicketts <stephen.ricketts12@gmail.com>
2020-10-21 15:09:00 -0400

    first commit

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const FilterScreen = (props) => {
+  <View>
+    <Text style={styles.screen}>This is the Filter Screen!</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FilterScreen;
